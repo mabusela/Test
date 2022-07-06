eccce462ab31829ea55fafc651b0af1b1bd87e81
Tebogo <tebogomabusela96@gmail.com>
2022-07-06 09:06:57 +0200

    Initial Commit

--- Test/src/testPackage/TestClass.java
+++ Test/src/testPackage/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("TortoiseGit is Working on my Machine!!");
+		System.out.println("TortoiseGit is Working on my Machine!");
 		
 
 	}
